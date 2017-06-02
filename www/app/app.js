f74d4b77739d2bc96ab2c03d88abf62fdd55cfab
Nattaphat Laoharawee <nattaphat.la@gmail.com>
2017-06-02 17:07:46 +0700

    product value pack vug

--- www/app/app.js
+++ www/app/app.js
@@ -86,7 +86,10 @@ angular.module('starter', [
 })
   .config(($urlRouterProvider,$ionicConfigProvider,$httpProvider,$provide,LoopBackResourceProvider) => {
     "ngInject";
-      
+      $ionicConfigProvider.backButton.text('Back').icon('ion-chevron-left');
+      $ionicConfigProvider.navBar.alignTitle("center"); //Places them at the bottom for all OS
+      $ionicConfigProvider.tabs.position("bottom"); //Places them at the bottom for all OS
+      $ionicConfigProvider.tabs.style("standard"); //Makes them all look the same across all OS
     //$locationProvider.html5mode(true)
     $provide.value("apiRoot", LoopBackResourceProvider.getUrlBase());
     $ionicConfigProvider.views.maxCache(0);
